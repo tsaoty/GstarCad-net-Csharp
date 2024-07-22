c5f2944e4aefea7bf1e5f55c36394775d6aef1e9
tsaoty <tsaoty@gmail.com>
2024-07-22 14:06:52 +0800

    chap03.05 end

---
+++ Chap03_net481/Commands/Commands.cs
@@ -0,0 +1,77 @@
+﻿using System.IO;
+using GrxCAD.ApplicationServices;
+using GrxCAD.DatabaseServices;
+using GrxCAD.EditorInput;
+using GrxCAD.Geometry;
+using GrxCAD.Runtime;
+using gsDotNetARX;
+
+namespace Commands
+{
+    public class Commands
+    {
+        [CommandMethod("ty_NetCommand")]
+        public void NetCommand()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            //提示用户选择圆的圆心
+            PromptPointResult result = ed.GetPoint("\n请选择圆心");
+            if (result.Status != PromptStatus.OK) return; // 如果选择失败，返回
+            Point3d pt = result.Value; // 获得用户选择的圆心            
+            // 命令字符串，画一个半径为10的圆
+            string cmd = string.Format("Circle {0},{1},{2} 10 ", pt.X, pt.Y, pt.Z);
+            cmd += "Zoom E "; // 命令字符串，缩放视图以使圆可见
+            // 获取NOMUTT系统变量，该变量用来控制是否禁止显示提示信息
+            object nomutt = Application.GetSystemVariable("NOMUTT");
+            // 命令字符串，表示在命令调用结束时，将NOMUTT设置为原始值
+            cmd += "_NOMUTT " + nomutt.ToString() + " ";
+            // 在命令调用之前，设置NOMUTT为1，表示禁止显示提示信息
+            Application.SetSystemVariable("NOMUTT", 1);
+            // 向AutoCAD发出cmd字符串所表示的命令
+            doc.SendStringToExecute(cmd, true, false, false);
+            // 显示对话框提示
+            Application.ShowAlertDialog("创建圆成功！");
+        }
+
+        [CommandMethod("ty_ComCommand", CommandFlags.Session)]
+        public void ComCommand()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            // 获取FILEDIA系统变量，该变量用来表示是否显示文件导航对话框
+            object filedia = Application.GetSystemVariable("FILEDIA");
+            // 设置FILEDIA=0，表示不显示文件导航对话框
+            Application.SetSystemVariable("FILEDIA", 0);
+            string filename = "C:\\test.dwg"; // 要保存的文件名
+            // 如果在C盘根目录下存在test.dwg，则删除
+            if (File.Exists(filename)) File.Delete(filename);
+            // 命令字符串，用来调用AutoCAD的另存为命令
+            string cmd = "SaveAs\n\n" + filename + "\n";
+            doc.SendCommand(cmd); // 调用COM的SendCommand函数，执行另存为命令
+            // 恢复FILEDIA系统变量为原始值
+            Application.SetSystemVariable("FILEDIA", filedia);
+        }
+
+        [CommandMethod("ty_ARXCommand")]
+        public void ARXCommand()
+        {
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Editor ed = doc.Editor;
+            // 提示用户选择要打断的对象兼作第一点
+            PromptEntityResult entRes = ed.GetEntity("\n请选择要打断的对象");
+            if (entRes.Status != PromptStatus.OK) return; // 若未选择，则返回
+            // 选择打断的第二点
+            PromptPointResult ptRes = ed.GetPoint("\n选择点");
+            if (ptRes.Status != PromptStatus.OK) return; // 若未选择，则返回
+            // ResultBuffer对象，用来存储命令、对象、点
+            ResultBuffer rb = new ResultBuffer();
+            rb.Add(new TypedValue(5005, "_Break")); // 字符串，表示命令
+            rb.Add(new TypedValue(5006, entRes.ObjectId)); // 实体的Id
+            rb.Add(new TypedValue(5009, ptRes.Value)); // 打断的第二点
+            ed.AcedCmd(rb); // 执行AutoCAD的打断命令
+            // 显示提示信息
+            Application.ShowAlertDialog("打断对象成功！");
+        }
+    }
+}
---
+++ Chap03_net481/MouseTip/MouseTip.cs
@@ -0,0 +1,76 @@
+﻿using GrxCAD.ApplicationServices;
+using GrxCAD.DatabaseServices;
+using GrxCAD.EditorInput;
+using GrxCAD.Runtime;
+using gsDotNetARX;
+
+namespace MouseTip
+{
+    public class MouseTip
+    {
+        //存储鼠标停留处的块参照的ObjectId
+        static ObjectIdCollection blockIds = new ObjectIdCollection();
+        static string blockName = "";//存储鼠标停留处的块名
+        [CommandMethod("ty_StartMonitor")]
+        public static void StartMonitor()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            //添加鼠标监视事件
+            ed.PointMonitor += new PointMonitorEventHandler(ed_PointMonitor);
+        }
+        static void ed_PointMonitor(object sender, PointMonitorEventArgs e)
+        {
+            string blockInfo = ""; //用于存储块参照的信息：名称和个数
+            //获取命令行对象（鼠标监视事件的发起者），用于获取文档对象
+            Editor ed = (Editor)sender;
+            Document doc = ed.Document;
+            //获取鼠标停留处的实体
+            FullSubentityPath[] paths = e.Context.GetPickedEntities();
+            using (Transaction trans = doc.TransactionManager.StartTransaction())
+            {
+                //如果鼠标停留处有实体
+                if (paths.Length > 0)
+                {
+                    //获取鼠标停留处的实体
+                    FullSubentityPath path = paths[0];
+                    BlockReference blockRef = trans.GetObject(path.GetObjectIds()[0], OpenMode.ForRead) as BlockReference;
+                    if (blockRef != null)//如果鼠标停留处为块参照
+                    {
+                        //获取块参照所属的块表记录并以写的方式打开
+                        ObjectId blockId = blockRef.BlockTableRecord;
+                        BlockTableRecord btr = trans.GetObject(blockId, OpenMode.ForRead) as BlockTableRecord;
+                        //获取属于同一块表记录的所有块参照
+                        ObjectIdCollection ids = btr.GetBlockReferenceIds(true, false);
+                        //若鼠标停留处的块参照的块表记录与上一次的不同
+                        if (blockName != btr.Name)
+                        {
+                            blockName = btr.Name;//重新设置块表记录名
+                            blockIds.UnHighlightEntities();//取消上一次块表记录的块参照的亮显
+                            blockIds.Clear(); //清空块参照ObjectId列表
+                            blockIds = ids;  //设置需要亮显的块参照的ObjectId列表
+                            blockIds.HighlightEntities();//亮显所有同名的块参照                            
+                        }
+                        blockInfo += "块名：" + btr.Name + "\n个数:" + blockIds.Count.ToString();
+                    }
+
+                }
+                trans.Commit();
+            }
+            if (blockInfo != "")
+            {
+                e.AppendToolTipText(blockInfo);//在鼠标停留处显示提示信息                  
+            }
+        }
+        [CommandMethod("ty_StopMonitor")]
+        public static void StopMonitor()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            //不亮显块参照
+            blockIds.UnHighlightEntities();
+            blockIds.Clear();
+            //停止鼠标监视事件
+            ed.PointMonitor -= new PointMonitorEventHandler(ed_PointMonitor);
+        }
+    }
+}
+
