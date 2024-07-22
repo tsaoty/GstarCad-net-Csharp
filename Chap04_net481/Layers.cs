13627d33191413c866f1b5de4573dad9ea031feb
tsaoty <tsaoty@gmail.com>
2024-07-22 15:13:25 +0800

    Chap04.1 Layer

---
+++ Chap04_net481/Layers.cs
@@ -0,0 +1,69 @@
+﻿using System.Linq;
+using GrxCAD.ApplicationServices;
+using GrxCAD.Colors;
+using GrxCAD.DatabaseServices;
+using GrxCAD.EditorInput;
+using GrxCAD.Runtime;
+using gsDotNetARX;
+namespace Layers
+{
+    public class Layers
+    {
+        [CommandMethod("ty_CreateLayer")]
+        public void CreateLayer()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            string layerName = "";//存储图层名
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                while (layerName == "")//如果用户没有输入图层名，则循环
+                {
+                    //提示用户输入图层名
+                    PromptResult pr = ed.GetString("请输入图层名称");
+                    if (pr.Status != PromptStatus.OK) return;
+                    try
+                    {
+                        //验证输入字符串是否符合符号表命名规则
+                        SymbolUtilityServices.ValidateSymbolName(pr.StringResult, false);
+                        layerName = pr.StringResult;//图层名
+                        //添加名为layerName的图层
+                        if (db.AddLayer(layerName) != ObjectId.Null)
+                        {
+                            ///提示用户输入图层颜色
+                            PromptIntegerResult pir = ed.GetInteger("请输入层的颜色值");
+                            if (pir.Status != PromptStatus.OK) return;
+                            //设置图层的颜色
+                            db.SetLayerColor(layerName, (short)pir.Value);
+                            //设置新添加的图层为当前层
+                            db.SetCurrentLayer(layerName);
+                            break;//添加图层成功，跳出循环
+                        }
+                    }
+                    catch (GrxCAD.Runtime.Exception ex)
+                    {
+                        //捕捉到异常,说明传入图层名不合法
+                        ed.WriteMessage(ex.Message + "\n");
+                    }
+                }
+                trans.Commit();
+            }
+        }
+
+        [CommandMethod("ty_DelRedLayer")]
+        public void DelRedLayer()
+        {
+            Database db = HostApplicationServices.WorkingDatabase;
+            using (Transaction trans = db.TransactionManager.StartTransaction())
+            {
+                //获取当前图形中所有颜色为红色的图层层名
+                var redLayers = (from layer in db.GetAllLayers()
+                                 where layer.Color == Color.FromColorIndex(ColorMethod.ByAci, 1)
+                                 select layer.Name).ToList();
+                //删除红色的图层
+                redLayers.ForEach(layer => db.DeleteLayer(layer));
+                trans.Commit();
+            }
+        }
+    }
+}
